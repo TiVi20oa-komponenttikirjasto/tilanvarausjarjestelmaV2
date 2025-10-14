a0038641f7ab4ba94fcce5c9a62d71a98c466f40
afkHuman <159439582+afkHuman@users.noreply.github.com>
2025-10-14 15:33:22 +0300

    Lisätty kommentteja ja docstringejä (ei valmis)

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import User, Space, Event
 
-# Register your models here.
-
 # Järjestelmänvalvojan rakenne MemberAdmin luokalle, joka hyödyntää UusiKayttaja-mallia
 class MemberAdmin(admin.ModelAdmin):
   """
@@ -10,12 +8,12 @@ class MemberAdmin(admin.ModelAdmin):
 
   Args:
       admin (ModelAdmin): The base admin class.
+      lsit_display (tuple): Fields to display in the admin list view.
+      prepopulated_fields (dict): Fields to auto-populate based on other fields.
   """
+
   list_display = ("idNumber", "firstname", "lastname", "email", "phone", "joined_date",)
   prepopulated_fields = {"slug": ("firstname", "lastname")}
-
-admin.site.register(User, MemberAdmin)
-
 
 # Järjestelmänvalvojan rakenne SpaceAdmin luokalle, joka hyödyntää Tilat-mallia
 class SpaceAdmin(admin.ModelAdmin):
@@ -23,12 +21,13 @@ class SpaceAdmin(admin.ModelAdmin):
 
   Args:
       admin (ModelAdmin): The base admin class.
+      list_display (tuple): Fields to display in the admin list view.
+      prepopulated_fields (dict): Fields to auto-populate based on other fields.
   """
+
   list_display = ("idNumber", "location", "publicity", "service_type", "type", "size", "capacity",)
   prepopulated_fields = {"slug": ("type", "location")}
 
-admin.site.register(Space, SpaceAdmin)
- 
 # Varausten hallinta adminissa
 class EventAdmin(admin.ModelAdmin):
   """
@@ -36,23 +35,48 @@ class EventAdmin(admin.ModelAdmin):
 
   Args:
     admin (ModelAdmin): The base admin class.
+    list_display (tuple): Fields to display in the admin list view.
+    list_filter (tuple): Fields to filter in the admin list view.
+    search_fields (tuple): Fields to search in the admin list view.
+    ordering (tuple): Default ordering for the admin list view.
   """
-  # Show date-only values in the list to avoid timezone/format confusion
+
   list_display = ('title', 'space', 'start_date', 'end_date')
   list_filter = ('space', 'title')
   search_fields = ('title',)
-  # Default ordering in admin list: earliest start first, tie-breaker by id
   ordering = ('start', 'id')
 
+  # Metodi joka palauttaa vain alkamis päivämäärän, jotta vältytään aikavyöhykkeisiin liittyviltä ongelmilta
   def start_date(self, obj):
-    # return a date-only representation to avoid timezone shifts in the admin list
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Palauttaa vain päivämäärän osan datetime-arvosta
     return obj.start.date() if obj.start else None
   start_date.admin_order_field = 'start'
   start_date.short_description = 'Start'
 
+  # Metodi joka palauttaa vain loppumis päivämäärän, jotta vältytään aikavyöhykkeisiin liittyviltä ongelmilta
   def end_date(self, obj):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Palauttaa vain päivämäärän osan datetime-arvosta
     return obj.end.date() if obj.end else None
   end_date.admin_order_field = 'end'
   end_date.short_description = 'End'
 
+
+admin.site.register(User, MemberAdmin)
+admin.site.register(Space, SpaceAdmin)
 admin.site.register(Event, EventAdmin)
